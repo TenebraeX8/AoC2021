f7f44598afb22e4ef73c4329887312085dade2e0
Tenebrae <TenebraeX8@gmx.at>
2021-12-03 10:52:01 +0100

    Day 3 in Kotlin

---
+++ src/com/tenebraex8/aoc2021/day3/3.kts
@@ -0,0 +1,46 @@
+import com.tenebraex8.aoc2021.firstSolution
+import com.tenebraex8.aoc2021.readLines
+import com.tenebraex8.aoc2021.secondSolution
+import java.util.stream.Collectors
+
+
+val content = "3.inp".readLines()
+val bitArray = Array(content.first().length) { 0 }
+content.forEach {
+    for(idx in it.indices) bitArray[idx] += if(it[idx] == '0') -1 else 1
+}
+val gamma = bitArray.map { if(it > 0) "1" else "0" }.stream().collect(Collectors.joining())
+val epsilon = bitArray.map { if(it <= 0) "1" else "0" }.stream().collect(Collectors.joining())
+(gamma.toInt(2) * epsilon.toInt(2)).firstSolution()
+
+var oxygen = 0
+var remnants = content
+val buckets = Pair(mutableListOf<String>(), mutableListOf<String>())
+for(idx in gamma.indices){
+    buckets.first.clear(); buckets.second.clear()
+    remnants.forEach {
+        if(it[idx] == '0') buckets.first.add(it)
+        else buckets.second.add(it)
+    }
+    remnants = if(buckets.first.size > buckets.second.size) buckets.first.toMutableList() else buckets.second.toMutableList()
+    if(remnants.size == 1){
+        oxygen = remnants.first().toInt(2)
+        break
+    }
+}
+var scrubber = 0
+remnants = content
+for(idx in gamma.indices){
+    buckets.first.clear(); buckets.second.clear()
+    remnants.forEach {
+        if(it[idx] == '0') buckets.first.add(it)
+        else buckets.second.add(it)
+    }
+    remnants = if(buckets.first.size <= buckets.second.size) buckets.first.toMutableList() else buckets.second.toMutableList()
+    if(remnants.size == 1){
+        scrubber = remnants.first().toInt(2)
+        break
+    }
+}
+
+(scrubber * oxygen).secondSolution()
