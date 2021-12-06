70cafaa1453a708ac8ce9508243e18c24fe42c2a
Tenebrae <TenebraeX8@gmx.at>
2021-12-06 12:49:56 +0100

    Day 6: removed duplicated code and used lambda

--- python/6.py
+++ python/6.py
@@ -3,5 +3,6 @@ from functools import reduce
 import numpy as np
 
 if __name__ == '__main__':
-	print(sum(reduce(lambda value, step: [value[(idx + 1) % len(value)] if idx != 6 else value[7] + value[0] for idx,x in enumerate(list(value[1:]) + list([value[0]]))], list(range(80)), np.bincount([int(x) for x in read_input(6)[0].split(",")], minlength=9))), sum(reduce(lambda value, step: [value[(idx + 1) % len(value)] if idx != 6 else value[7] + value[0] for idx,x in enumerate(list(value[1:]) + list([value[0]]))], list(range(256)), np.bincount([int(x) for x in read_input(6)[0].split(",")], minlength=9))))
+	#print(sum(reduce(lambda value, step: [value[(idx + 1) % len(value)] if idx != 6 else value[7] + value[0] for idx,x in enumerate(list(value[1:]) + list([value[0]]))], list(range(80)), np.bincount([int(x) for x in read_input(6)[0].split(",")], minlength=9))), sum(reduce(lambda value, step: [value[(idx + 1) % len(value)] if idx != 6 else value[7] + value[0] for idx,x in enumerate(list(value[1:]) + list([value[0]]))], list(range(256)), np.bincount([int(x) for x in read_input(6)[0].split(",")], minlength=9))))
+	print([(lambda time: sum(reduce(lambda value, _: [value[(idx + 1) % len(value)] if idx != 6 else value[7] + value[0] for idx,_ in enumerate(list(value[1:]) + list([value[0]]))], list(range(time)), np.bincount([int(x) for x in read_input(6)[0].split(",")], minlength=9))))(stop) for stop in [80, 256]])
 	
