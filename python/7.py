410c8b823d2fd6bef2bb1a02b1a2fd5dde5dfc57
Tenebrae <TenebraeX8@gmx.at>
2021-12-07 09:06:29 +0100

    Day 7 python: using sum(x) instead reduce(a+b, x, 0)

--- python/7.py
+++ python/7.py
@@ -4,4 +4,4 @@ import math
 import numpy as np
 
 if __name__ == '__main__':
-	print(sum([abs(nr) for nr in np.array([int(x) for x in read_input(7)[0].split(",")] - np.median([int(x) for x in read_input(7)[0].split(",")]))]), sum(reduce(lambda a, b: a + b, range(1, y+1),0) for y in [int(abs(nr)) for nr in np.array([int(x) for x in read_input(7)[0].split(",")]) - math.floor(np.mean([int(x) for x in read_input(7)[0].split(",")]))]))
+	print(sum([abs(nr) for nr in np.array([int(x) for x in read_input(7)[0].split(",")] - np.median([int(x) for x in read_input(7)[0].split(",")]))]), sum(sum(range(y+1)) for y in [int(abs(nr)) for nr in np.array([int(x) for x in read_input(7)[0].split(",")]) - math.floor(np.mean([int(x) for x in read_input(7)[0].split(",")]))]))
