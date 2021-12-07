51e9f6634b94f10d2edbb99f432ae5378a3df679
Tenebrae <TenebraeX8@gmx.at>
2021-12-07 09:09:21 +0100

    Day 7 Kotlin: changed fold to sum

--- src/com/tenebraex8/aoc2021/day7/7.kts
+++ src/com/tenebraex8/aoc2021/day7/7.kts
@@ -1,6 +1,5 @@
 import com.tenebraex8.aoc2021.*
 import kotlin.math.abs
-import kotlin.math.ceil
 import kotlin.math.floor
 
 val input = "7.inp".readContent().splitAndRemoveEmpty(",").map { it.toInt() }
@@ -8,7 +7,6 @@ val median = input.median()
 input.map { abs(it.toDouble() - median) }.sum().toInt().firstSolution()
 
 val mean = input.sum().toDouble() / input.size.toDouble()
-//val meanInt = if(mean - mean.toInt().toDouble() >= 0.5) ceil(mean).toInt() else floor(mean).toInt() //96592329
 val meanInt = floor(mean).toInt()       //I donno why, but seems like they want floor for this solution
 
-input.map { (1..abs(it-meanInt)).fold(0){acc, i -> acc + i} }.sum().secondSolution()
+input.map { (1..abs(it-meanInt)).sum() }.sum().secondSolution()
